6027a083cb71bc9a51a599744363ff20a312683b
santy8989 <santygarces@gmail.com>
2024-10-26 15:43:50 -0300

    Feature: Add Pagination and Sorting to  Table with Responsive Design
    -Integrated Angular Material pagination into the hero table in HeroListComponent to manage data display across multiple pages.
    -Added column sorting functionality, allowing users to sort heroes by various columns.
    -Enhanced pagination component with responsive styling

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,29 +1,63 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule,MatPaginatorIntl} from '@angular/material/paginator';
+
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatTableModule, MatButtonModule, MatIconModule, MatPaginatorModule, MatSortModule  ],
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
+  constructor(private paginatorIntl: MatPaginatorIntl) {}
+  
   @Input() columns: { header: string; field: string }[] = [];
   @Input() dataSource: any[] = [];
   @Input() actions?: { icon: string; color?: string; callback: (row: any) => void }[] = [];
-
-
+  
   displayedColumns: string[] = [];
+  dataSourceWithPagination = new MatTableDataSource<any>(this.dataSource);
+  
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   ngOnInit(): void {
+    this.paginatorIntl.itemsPerPageLabel = 'Ítems por página';
+    this.paginatorIntl.nextPageLabel = 'Siguiente página';
+    this.paginatorIntl.previousPageLabel = 'Página anterior';
+    this.paginatorIntl.firstPageLabel = 'Primera página';
+    this.paginatorIntl.lastPageLabel = 'Última página';
+    this.paginatorIntl.getRangeLabel = (page: number, pageSize: number, length: number) => {
+      if (length === 0 || pageSize === 0) {
+        return `0 de ${length}`;
+      }
+      const startIndex = page * pageSize;
+      const endIndex = Math.min(startIndex + pageSize, length);
+      return `${startIndex + 1} - ${endIndex} de ${length}`;
+    };
+    this.paginatorIntl.changes.next();
+    
     this.displayedColumns = this.columns.map(col => col.field);
     if (this.actions && this.actions.length) {
       this.displayedColumns.push('actions');
     }
+    this.dataSourceWithPagination = new MatTableDataSource(this.dataSource);
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSourceWithPagination.paginator = this.paginator;
+    this.dataSourceWithPagination.sort = this.sort;
+  }
+
+  ngOnChanges(): void {
+    this.dataSourceWithPagination.data = this.dataSource;
   }
 
   onAction(action: (row: any) => void, row: any): void {
