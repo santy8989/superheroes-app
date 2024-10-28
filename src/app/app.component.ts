9defb030c0956f4be4b086ae2a14021900c18c62
santy8989 <santygarces@gmail.com>
2024-10-28 02:09:34 -0300

    feat: add header and footer components with layout adjustments in AppComponent
    - Created  HeaderComponent.
    - Created  FooterComponent .
    - Implemented HeaderComponent and FooterComponent in AppComponent layout.
    - Adjusted AppComponent styles to ensure router-outlet expands to full height between header and footer.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { HeroListComponent } from './components/hero-list/hero-list.component';
 import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
-
-
-
-
-
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule,HeroListComponent,LoadingSpinnerComponent],
+  imports: [RouterOutlet, MatButtonModule, HeroListComponent, LoadingSpinnerComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+
+
+
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatToolbarModule,MatIconModule,MatButtonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  constructor(private router: Router) {}
+
+  navigateToHome(): void {
+    console.log("hola")
+    this.router.navigate(['/']);
+  }
+}
